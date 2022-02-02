4a26a5d2903d12fa5c1b89f493903a8582a7dd8c
Mackenzie Goodwin <mackenziejgoodwin@gmail.com>
2022-02-02 09:33:41 -0800

    Updated spelling mistakes on projects page for resonator

--- src/data/projects/projects.js
+++ src/data/projects/projects.js
@@ -8,8 +8,8 @@ const data = [
     image: '/images/projects/microplastics.jpg',
     date: '2022-04',
     desc:
-      'Developed a device to microbiologists detect microplastic to determine pollution in an ecosystem. '
-      + 'This used a 6GHz cavity resonator to measure dialetric properties of materials. '
+      'Developed a device to aid microbiologists in detecting microplastic in water to determine pollution in an ecosystem. '
+      + 'This uses a 6GHz cavity resonator to measure dialetric properties of materials. '
       + 'Everything was simulated in Ansys HFSS and tested using a microfluidic pump and VNA.',
   },
 ];
