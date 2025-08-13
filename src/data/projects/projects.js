c4e0a432c28d984958b0dc4ed390db228812da1d
mackenzieg <mackenziejgoodwin@gmail.com>
2025-08-12 22:37:51 -0700

    Fixed up the projects subtext

--- src/data/projects/projects.js
+++ src/data/projects/projects.js
@@ -9,19 +9,19 @@ const projectCards = [
     image: '/images/projects/microplastics.jpg',
     date: '2022-04',
     desc:
-      'Developed a device to aid microbiologists in detecting microplastic in water to determine pollution in an ecosystem. '
-      + 'This uses a 6GHz cavity resonator to measure dialetric properties of materials. '
-      + 'Everything was simulated in Ansys HFSS and tested using a microfluidic pump and VNA.',
+      'Developed a device to aid microbiologists in detecting microplastics in water to determine pollution in an ecosystem. '
+      + 'Uses a 6 GHz cavity resonator to measure dielectric properties of materials. '
+      + 'Simulated entirely in Ansys HFSS and tested with a microfluidic pump and VNA.',
   },
   {
     projectId: 'mmwave-radar',
     title: 'mmWave 60GHz Radar Vital Sign Detection',
-    subtitle: 'At a distance breath-rate detection',
+    subtitle: 'Breath-rate detection at a distance',
     image: '/images/projects/mmwaveradar.jpg',
     date: '2020-04',
     desc:
-      'Designed a system using the Infineon 60GHz mmWave radar for detecting breath rate of patients. This is for old age homes '
-      + 'to automatically collect health metric of the residents.',
+      'Designed a system using the Infineon 60 GHz mmWave radar for detecting breath rates of patients. '
+      + 'Intended for use in care homes to automatically collect health metrics from residents.',
   },
   {
     projectId: 'rf-pulser',
@@ -30,29 +30,28 @@ const projectCards = [
     image: '/images/projects/rfpulser.jpg',
     date: '2021-02',
     desc:
-      'Designed a 6GHz VCO with AM modulation and 20dBm output power. This was just for fun as I was taking a course on '
-      + 'Signals & Systems as well as RF Systems.',
+      'Designed a 6 GHz VCO with AM modulation and 20 dBm output power. '
+      + 'Created as a side project while taking courses on Signals & Systems and RF Systems.',
   },
   {
     projectId: 'ecg',
     title: '3-Lead ECG With ARM Dev Board',
-    subtitle: 'For detecting arrhythmia in heart beats',
+    subtitle: 'For detecting arrhythmia in heartbeats',
     image: '/images/projects/ecg.jpg',
     date: '2020-08',
     desc:
-      'Designed a build a ECG for detecting irregular heat beats. Later this will be developed to detect other conditions '
-      + 'using machine learning.',
+      'Designed and built a 3-lead ECG for detecting irregular heartbeats. '
+      + 'Planned future development includes detecting other conditions using machine learning.',
   },
   {
     projectId: 'pkm-go',
     title: 'PokemonGo Bot',
-    subtitle: 'Automatically catches pokemon, walks, evolves and xp farms',
+    subtitle: 'Automatically catches Pokémon, walks, evolves, and XP farms',
     image: '/images/projects/pokemongo.jpg',
     date: '2017-01',
     desc:
-      'At the time I was really interested in PokemonGo and wanted a way to level up without actually playing. '
-      + 'As a fun project I decided to design and build my own auto xp farming PokemonGo bot. This included a path finding '
-      + 'bot to automatically walk and pickup pokemon.',
+      'At the time, I was really into PokemonGo and wanted a way to level up without constant playing. '
+      + 'As a fun project, I designed and built my own auto XP farming bot, including a pathfinding system to automatically walk and catch Pokémon.',
   },
 ];
 
