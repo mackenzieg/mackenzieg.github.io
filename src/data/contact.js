7d22784a1887ea487dbc40162fab3a5eb81a3135
mackenzieg <mackenziejgoodwin@gmail.com>
2026-08-31 22:54:29 -0700

    Removed unused icons

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,6 +1,4 @@
 import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
-import { faFacebookF } from '@fortawesome/free-brands-svg-icons/faFacebookF';
-import { faInstagram } from '@fortawesome/free-brands-svg-icons/faInstagram';
 import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
 // import { faAngellist } from '@fortawesome/free-brands-svg-icons/faAngellist';
 // import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
